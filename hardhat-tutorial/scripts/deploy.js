59637f6464f5c9a1dbe59f61a69521d4128c03c4
Sion-Park <81695580+Sion-Park@users.noreply.github.com>
2023-11-28 19:46:27 +0900

    Update deploy.js

--- hardhat-tutorial/scripts/deploy.js
+++ hardhat-tutorial/scripts/deploy.js
@@ -2,9 +2,7 @@ async function main(){
     const [deployer]=await ethers.getSigners();
     const My_Token = await ethers.getContractFactory("My_Token");
     const mytoken = await My_Token.deploy();
-    await mytoken.deployed();
-
-    console.log(mytoken.address);
+    console.log(mytoken.target);
     
 }
 main()
